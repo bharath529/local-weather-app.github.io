036bfd4df47ec34bc829699cf3d28ad53f409870
bharathbheemireddy <bharath.bheemireddy29@gmail.com>
2017-07-21 16:25:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var key      ="b1b15e88fa797225412429c1c50c122a1";
 $(function() {
    var lat="";
   var lon="";
@@ -10,15 +11,16 @@ $(function() {
   $.getJSON(api+lat+"&lon="+lon,function(value){
       $("#location").html(value.name);
        $("#temp").html(value.main.temp);
+       $("#temp").append(" celsius");
   $("#description").html(value.weather[0].description);
-    $('#icon').html('<img src="'+value.weather[0].icon+'" >');
+    $('#icon').html('<img src="'+value.weather[0].icon+'" />');
     var temp = value.main.temp;
     var F = Math.round((temp*9/5)+32);
     $("#btn").click(function(){
     $("#temp").html(F);
+    $("#temp").append(" fahrenheit");
   });
   });
    });
 
 });
-
