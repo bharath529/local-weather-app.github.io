69a3750345e5c0ef2f2231cd39d924bf0917707e
bharathbheemireddy <bharath.bheemireddy29@gmail.com>
2017-07-21 16:12:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+$(function() {
+   var lat="";
+  var lon="";
+  var icon="";
+  var api="https://fcc-weather-api.glitch.me/api/current?lat=";
+   var api1="https://api.openweathermap.org/data/2.5/weather?lat=";  navigator.geolocation.getCurrentPosition(function(position) {
+   lat=position.coords.latitude ;
+   lon=position.coords.longitude;
+
+  $.getJSON(api+lat+"&lon="+lon,function(value){
+      $("#location").html(value.name);
+       $("#temp").html(value.main.temp);
+  $("#description").html(value.weather[0].description);
+    $('#icon').html('<img src="'+value.weather[0].icon+'" >');
+    var temp = value.main.temp;
+    var F = Math.round((temp*9/5)+32);
+    $("#btn").click(function(){
+    $("#temp").html(F);
+  });
+  });
+   });
+
+});
 
